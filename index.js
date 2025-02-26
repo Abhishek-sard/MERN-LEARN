7a2018ea795c020b503387e944d09674bd234749
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-02-26 21:14:38 +0545

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { addToCart, changeQty } = require("./cartModule");
+// const { addToCart, changeQty } = require("./cartModule");
 
 // console.log("Welcome to home");
 // console.log(10 + 20);
@@ -13,39 +13,57 @@ const { addToCart, changeQty } = require("./cartModule");
 // console.log("Welcome");
 
 
-let http = require("http");
+// let http = require("http");
 
-let server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
+// let server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "application/json" });
 
-    if (req.url == "/news") {  
-        let obj = {
-            status: 1,
-            data: [
-                { newsTitle: "ws", newsDes: "Abhishek" },
-                { newsTitle: "Abhishek Sardar", newsDes: "TTP hello" }
-            ]
-        };
-        return res.end(JSON.stringify(obj)); 
-    }
+//     if (req.url == "/news") {  
+//         let obj = {
+//             status: 1,
+//             data: [
+//                 { newsTitle: "ws", newsDes: "Abhishek" },
+//                 { newsTitle: "Abhishek Sardar", newsDes: "TTP hello" }
+//             ]
+//         };
+//         return res.end(JSON.stringify(obj)); 
+//     }
 
-    if (req.url == "/about") {
-        return res.end(JSON.stringify({ message: "About Us Page" }));
-    }
+//     if (req.url == "/about") {
+//         return res.end(JSON.stringify({ message: "About Us Page" }));
+//     }
 
-    if (req.url == "/course") {
-        return res.end(JSON.stringify({ message: "Courses Page" }));
-    }
+//     if (req.url == "/course") {
+//         return res.end(JSON.stringify({ message: "Courses Page" }));
+//     }
 
-    if (req.url == "/") {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        return res.end("Welcome Abhishek");
-    }
+//     if (req.url == "/") {
+//         res.writeHead(200, { "Content-Type": "text/plain" });
+//         return res.end("Welcome Abhishek");
+//     }
 
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
-});
+//     res.writeHead(404, { "Content-Type": "text/plain" });
+//     res.end("404 Not Found");
+// });
 
-server.listen(8000, () => {
-    console.log("Server running at http://localhost:8000");
-});
+// server.listen(8000, () => {
+//     console.log("Server running at http://localhost:8000");
+// });
+
+
+let express=require("express")
+
+let app=express()
+
+app.get("/",(req, res)=>{
+    res.send({status:1, msg: "home page API"})
+})
+
+app.get('/news', (req,res)=>{
+    res.send({status:1, msg: "hello abhishek"})
+})
+
+app.get('/about', (req,res)=>{
+    res.send({status:3, msg: "hello abhishek sardar"})
+})
+app.listen("8000")
